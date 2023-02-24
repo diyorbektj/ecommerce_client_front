e05aaa28199dc99f504939ce01bc8601a9567194
DIYORBEK <diyorbek5300@gmail.com>
2023-02-24 11:38:54 +0500

    test

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,91 +1,92 @@
-<template style="max-width: 800px; margin: 0 auto">
+<template>
   <Head>
     <Title>{{ product.name }}</Title>
     <Meta name="description" :content="product.description"/>
   </Head>
 <v-card>
 <!--  <p><img style="max-width: 100%; height: auto" src="https://img.joomcdn.net/3a471d818df6d088b956251e491cf6f707c9e3c0_400_400.jpeg" alt=""></p>-->
-  <div class="images-res" style="display: flex; justify-content: center; align-items: center;">
-    <section class="gallery product-images">
-      <div v-for="(item, index) in product.images" :key="index" class="gallery__item" style="display: flex; justify-content: center">
-        <input type="radio" :id='"img-"+index' checked name="gallery" class="gallery__selector"/>
-        <img style="width: 100%; height: 300px; padding-left: 6px; padding-right: 6px; padding-top: 12px" class="gallery__img" :src="item.path" alt=""/>
-        <label :for='"img-"+index' class="gallery__thumb"><img style="width: 100px; height: 100px" :src="item.path" alt=""/></label>
-      </div>
+ <div class="container-card">
+   <div style="padding-top: 40px">
+     <div class="modal-content">
+       <div class="modal-nav">
+         <div class="modal-nav-item" v-for="(image, index) in product.images" :key="index" @click="changeSlide(index, image.path)">
+           <div><img class="img-item" :src="image.path" alt="Product Image"></div>
+         </div>
+       </div>
+       <div><img class="img-cover" :src="image" alt="Product Image"></div>
+     </div>
+   </div>
+   <div>
+     <v-col>
+       <div class="pa-2">
+         <p>{{ product.name }}</p>
+       </div>
+       <div style="display: flex;justify-content: space-between">
+         <p><v-rating
+             v-model="rating"
+             :value="rating"
+             background-color="green lighten-2"
+             color="green"
+             size="25"
+             empty-icon="$ratingFull"
+             half-increments
+             hover
+             large
+         ></v-rating></p>
+         <p style="color: #ff0000">
+           <v-icon>mdi-heart-outline</v-icon>
+         </p>
+       </div>
+       <div style="font-size: 12px; margin-top: 8px;">{{ product.description }}</div>
+       <div class="py-4">
+         <div class="py-4">
+           <button @click="Decrement" style="padding: 2px 10px; background: #ffffff; border: 1px solid #c6c6c6; border-radius: 9999999px">-</button>
+           <input :value="quantity" type="number" style="width: 40px; height: 32px; border: 1px solid #c6c6c6; border-radius: 15px; padding-left: 8px;">
+           <button @click="Increment" style="padding: 2px 10px; background: #ffffff; border: 1px solid #c6c6c6; border-radius: 9999999px">+</button>
+         </div>
+         <p style="font-size: 25px">{{product.price}} сомони</p>
+       </div>
+       <div style="display: flex;justify-content: space-between">
+         <v-btn @click="AddBasket(product.id)" class="px-8" style="font-size: 16px" color="success">В корзину</v-btn>
+         <v-btn disabled class="px-6" style="font-size: 16px" color="red">Купить сейчас</v-btn>
+       </div>
+       <div v-if="errors" style="margin-top: 10px; margin-bottom: 10px">
+         <v-alert
+             color="error"
+             icon="$error"
+             title=""
+             text="Пожалуйста выберите цвет и размер товара"
+         ></v-alert>
+       </div>
+       <div>
+         <div>
+           <div class="py-4" style="font-size: 20px">Цвет: {{ selected_color }}</div>
 
-    </section>
-    <div>
-      <div></div>
-    </div>
-  </div>
-  <v-col>
-    <div class="pa-2">
-      <p>{{ product.name }}</p>
-    </div>
-    <div style="display: flex;justify-content: space-between">
-      <p><v-rating
-          v-model="rating"
-          :value="rating"
-          background-color="green lighten-2"
-          color="green"
-          size="25"
-          empty-icon="$ratingFull"
-          half-increments
-          hover
-          large
-      ></v-rating></p>
-      <p style="color: #ff0000">
-        <v-icon>mdi-heart-outline</v-icon>
-      </p>
-    </div>
-    <div style="font-size: 12px; margin-top: 8px;">{{ product.description }}</div>
-    <div class="py-4">
-      <div class="py-4">
-        <button @click="Decrement" style="padding: 2px 10px; background: #ffffff; border: 1px solid #c6c6c6; border-radius: 9999999px">-</button>
-        <input :value="quantity" type="number" style="width: 40px; height: 32px; border: 1px solid #c6c6c6; border-radius: 15px; padding-left: 8px;">
-        <button @click="Increment" style="padding: 2px 10px; background: #ffffff; border: 1px solid #c6c6c6; border-radius: 9999999px">+</button>
-      </div>
-      <p style="font-size: 25px">{{product.price}} сомони</p>
-    </div>
-    <div style="display: flex;justify-content: space-between">
-      <v-btn @click="AddBasket(product.id)" class="px-8" style="font-size: 16px" color="success">В корзину</v-btn>
-      <v-btn disabled class="px-6" style="font-size: 16px" color="red">Купить сейчас</v-btn>
-    </div>
-    <div v-if="errors" style="margin-top: 10px; margin-bottom: 10px">
-      <v-alert
-          color="error"
-          icon="$error"
-          title=""
-          text="Пожалуйста выберите цвет и размер товара"
-      ></v-alert>
-    </div>
-    <div>
-      <div>
-        <div class="py-4" style="font-size: 20px">Цвет: {{ selected_color }}</div>
+           <div style="display: flex; align-items: center">
+             <div v-for="color in colors">
+               <button @click="Select_colors(color.value)" v-if="selected_color === color.value" :style="'background:'+color.color_code+'; border-radius: 999999px; height: 35px; width: 35px; border: 3px solid green; margin: 4px'">
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="color: #00d028;">
+                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                 </svg>
 
-        <div style="display: flex; align-items: center">
-          <div v-for="color in colors">
-            <button @click="Select_colors(color.value)" v-if="selected_color === color.value" :style="'background:'+color.color_code+'; border-radius: 999999px; height: 35px; width: 35px; border: 3px solid green; margin: 4px'">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="color: #00d028;">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-              </svg>
-
-            </button>
-            <button @click="Select_colors(color.value)" v-else :style="'background:'+color.color_code+'; border-radius: 999999px; height: 35px; width: 35px; border: 2px solid #b7b7b7; margin: 4px'"></button>
-          </div>
-        </div>
-      </div>
-      <div v-if="sizes !=='NULL' || sizes !==''">
-        <div class="py-4" style="font-size: 20px">Размер: {{ selected_size }}</div>
-        <div style="display: flex; align-items: center">
-          <div v-for="size in sizes">
-            <button @click="Select_sizes(size.value)" v-if="selected_size === size.value"  style="background: #e7e7e7; padding: 6px 8px 6px 8px; font-size: 16px; font-weight: 300; margin: 6px; border-radius: 8px; border: 2px solid #000000">{{ size.value }}</button>
-            <button @click="Select_sizes(size.value)" v-else style="background: #e7e7e7; padding: 6px 8px 6px 8px; font-size: 16px; font-weight: 300; margin: 6px; border-radius: 8px;">{{ size.value }}</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </v-col>
+               </button>
+               <button @click="Select_colors(color.value)" v-else :style="'background:'+color.color_code+'; border-radius: 999999px; height: 35px; width: 35px; border: 2px solid #b7b7b7; margin: 4px'"></button>
+             </div>
+           </div>
+         </div>
+         <div v-if="sizes !=='NULL' || sizes !==''">
+           <div class="py-4" style="font-size: 20px">Размер: {{ selected_size }}</div>
+           <div style="display: flex; align-items: center">
+             <div v-for="size in sizes">
+               <button @click="Select_sizes(size.value)" v-if="selected_size === size.value"  style="background: #e7e7e7; padding: 6px 8px 6px 8px; font-size: 16px; font-weight: 300; margin: 6px; border-radius: 8px; border: 2px solid #000000">{{ size.value }}</button>
+               <button @click="Select_sizes(size.value)" v-else style="background: #e7e7e7; padding: 6px 8px 6px 8px; font-size: 16px; font-weight: 300; margin: 6px; border-radius: 8px;">{{ size.value }}</button>
+             </div>
+           </div>
+         </div>
+       </div>
+     </v-col>
+   </div>
+ </div>
 </v-card>
   <v-snackbar
       v-model="snackbar"
@@ -129,6 +130,9 @@ export default {
     selected_size: "",
     quantity: 1,
     errors: false,
+    modalOpen: false,
+    currentIndex: 0,
+    image: ""
   }),
   mounted() {
     axios.get(BASE_URL+`/api/product`).then(response => {
@@ -141,11 +145,16 @@ export default {
       this.product = response.data.data
       this.colors = response.data.data.colors
       this.sizes = response.data.data.sizes ?? 'NULL'
+      this.image = response.data.data.images[0].path
     }).catch(err => {
       console.log(err);
     });
   },
   methods: {
+    changeSlide(index, path) {
+      this.image = path
+      this.currentIndex = index;
+    },
     AddBasket(id){
       if(this.selected_color !== '' && this.selected_size === 'NULL' || this.selected_size !== '')
       {
@@ -173,87 +182,116 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-@use "sass:math";
-* {
-  box-sizing: border-box;
-}
 
-$max-img-width: 350px;
-$max-img-height: 400px;
-
-img {
-  max-width: 100%;
-  //vertical-align: top;
-}
-
-.gallery {
-  display: flex;
-  margin: 0px auto;
-  max-width: 100%;
-  position: relative;
-  padding-top: 300px;
-
-  @media screen and (min-width: 400px){
-    padding-top: 350px;
-  }
-
-  @media screen and (min-width: 600px){
-    padding-top: 360px;
-  }
-
-  &__img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
-  }
-
-  &__thumb {
-    padding-top: 6px;
-    margin: 6px;
-    display: block;
-  }
-
-  &__selector {
-    position: absolute;
-    opacity: 0;
-    visibility: hidden;
-
-    &:checked {
-      + .gallery__img {
-        opacity: 1;
-      }
-      ~ .gallery__thumb > img {
-        box-shadow: 0 0 0 3px rgb(5,177,135);;
-      }
-    }
-  }
-
-
-}
-</style>
 <style scoped>
-.images_res{
 
+.swiper-slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  cursor: pointer;
 }
-@media only screen and (max-width: 320px) {
-  .images_res{
-    padding: 116px;
+
+.modal-content {
+  display: flex;
+  max-width: 400px;
+  justify-content: space-between;
+  margin: auto;
+  width: 95%;
+  height: 95%;
+}
+
+.modal-nav {
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  box-sizing: content-box;
+  margin-right: 8px;
+  padding-right: 1.1rem;
+  width: calc(16.6%);
+  height: 340px;
+  scroll-behavior: smooth;
+}
+.modal-nav::-webkit-scrollbar {
+  width: 0px;
+}
+
+
+.modal-nav-item img {
+  width: 60px;
+  height: 60px;
+  object-fit: cover;
+  cursor: pointer;
+}
+.container-card {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+}
+.img-cover{
+  width: 273px;
+  height: 365px
+}
+.img-item{
+  width: 8000px;
+  height: 100px
+}
+@media (max-width: 410px) {
+  .container-card {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+  }
+  .img-cover{
+    width: 220px;
+    height: 320px
+  }
+  .modal-nav{
+    height: 320px;
+  }
+  .img-item{
+    width: 85px;
+    height: 110px
+  }
+}
+@media (min-width: 410px) and  (max-width: 820px) {
+  .container-card {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+  }
+  .modal-content{
+    max-width: 100%;
+  }
+  .img-cover{
+    width: 318px;
+    height: 424px
+  }
+  .modal-nav{
+    height: 424px;
+  }
+  .img-item{
+    width: 85px;
+    height: 110px
   }
 }
 
 /* iPads (portrait and landscape)  */
-@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
-  .images_res{
-    padding: 8px;
+@media (min-width: 820px){
+  .img-cover{
+    width: 340px;
+    height: 545px
   }
-}
-.product-images {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
+  .modal-nav{
+    height: 545px;
+  }
+  .img-item{
+    width: 80px;
+    height: 80px
+  }
+  .container-card {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 a{
   color: #000000;
